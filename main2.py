bd5de561a31f6de78e9c7413ad310b2301a1c2b7
imranvee <vilcassim1@gmail.com>
2020-06-08 19:20:18 +1000

    Added in Error function and comments on how to go forward tomorrow

--- main2.py
+++ main2.py
@@ -3,40 +3,29 @@ import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 
-# Link to the BOM Site and details
-url = "http://www.bom.gov.au/places/vic/melbourne/forecast/detailed/"  # would need a forloop to loop thorugh all
-# of these
-city = "Melbourne"  # Would need this to go into the CSV so that each DF will have this as a heading, there is a
-# part of this in the write function, would have to be an input might have to call the requests.get as a seperate
-# function and the output of this could then go into the table function
-timestamp = datetime.now()
-timestamp = timestamp.strftime('%Y-%m-%d %H:%M:%S')
 
-errorlist = []
-try:
-    webpage = requests.get(url)
-except requests.ConnectionError:
-    error = "No Internetss"
-    errorlist.append(error)
-    dffault = pd.DataFrame(errorlist, columns=["column"])
-    dffault.to_csv('errorlist.csv', index=False)
-    exit()  # include date and time in the file name like you did for the normal CSV
+# defining functions
 
-# insert error handling here
-soup = BeautifulSoup(webpage.text, 'html.parser')
+def pageparse(link):
+    errorlist = []
+    try:
+        webpage = requests.get(link)
+    except requests.ConnectionError:
+        error = "No Internetss"
+        errorlist.append(error)
+        dffault = pd.DataFrame(errorlist, columns=["column"])
+        dffault.to_csv('errorlist.csv', index=False)
+        exit()  # include date and time in the file name like you did for the normal CSV
 
-# insert error handling here
-# Grabs the first table in the url, which is the forecast data
-table = soup.find_all('table')[0]
+    soup = BeautifulSoup(webpage.text, 'html.parser')
+    table = soup.find_all('table')[0]
 
-# This part is designed to loop through the table from the URL
+    return table, soup, errorlist
 
-loop = 0  # This part is to differentiate between the first and second loops
-innerloop = 0
+def tablemaker (table, timestamp, city):
 
-# You could write this as a separate function and call it for each city.
-with open('BOM Forecast ' + timestamp + '.csv', 'a',
-          encoding='utf-8-sig') as f:  # would have to move this out of the function
+    loop = 0  # This part is to differentiate between the first and second loops
+    innerloop = 0
     for element in table.find_all('tr'):  # from here onwards this could go into a separate function (table function)
 
         chancelist = []
@@ -59,4 +48,44 @@ with open('BOM Forecast ' + timestamp + '.csv', 'a',
     f.write("\n")
     print(df)
 
-#end
+    return
+
+
+# Link to the BOM Site and details
+# Putting them into a list
+
+Geo_list = []
+
+url1 = "http://www.bom.gov.au/places/vic/melbourne/forecast/detailed/"
+url2 = "http://www.bom.gov.au/places/nsw/sydney/forecast/detailed/"
+city1 = "Melbourne"
+city2 = "Sydney"
+
+entry = {
+            'url': url1,
+            'city': city1,
+    }
+Geo_list.append(entry)
+
+entry = {
+            'url' : url2,
+            'city': city2
+    }
+Geo_list.append(entry)
+
+
+timestamp = datetime.now()
+timestamp = timestamp.strftime('%Y-%m-%d %H:%M:%S')
+
+with open('BOM Forecast ' + timestamp + '.csv', 'a',
+          encoding='utf-8-sig') as f:
+
+    for item in Geo_list:
+        print(item['url'])
+
+        data = pageparse(item['url'])
+        table = data[0]
+        tablemaker(table,timestamp,item['city'])
+
+
+
